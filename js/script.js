bb5cb2bdf0aa0728118f1fdeb02b73a92a9a1a29
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-01-07 13:58:04 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const newTask = document.getElementById("addtask");
+const list = document.getElementById("list");
+const row = document.getElementsByClassName("row");
+
+
+newTask.addEventListener("click", () => {
+
+    const input = document.getElementById("todoinput");
+
+    if (!input.value) {
+        alert("Please fill the input")
+        return;
+    }
+    var task = input.value;
+
+    const div = document.createElement("div");
+    div.className = "itemList mt-2";
+    list.appendChild(div);
+
+    const li = document.createElement("li");
+    li.className = "newList";
+    li.innerText = task;
+    div.appendChild(li);
+
+    const edit = document.createElement("button");
+    edit.innerHTML = "Edit";
+    edit.className = "btn btn-warning";
+    div.appendChild(edit);
+
+    const del = document.createElement("button");
+    del.innerText = "Delete";
+    del.className = "btn btn-danger";
+    div.appendChild(del);
+
+    input.value = "";
+
+    del.addEventListener("click", () => {
+        list.removeChild(div)
+    })
+
+    edit.addEventListener("click", () => {
+        if (edit.innerText.toLowerCase() == "edit") {
+            li.contentEditable = true;
+            li.style.background = "#000"
+            edit.innerText = "Save";
+        } else {
+            li.contentEditable = false;
+            li.style.background = "none"
+            edit.innerText = "Edit";
+        }
+    })
+
+})
